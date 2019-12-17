6c18fb74077881d60f0f384cbaa0b15dc7995eb8
wyang19a <wyang19a@gmail.com>
2019-12-17 08:48:11 -0500

    Unknown change..

--- app/serializers/ingredient_serializer.rb
+++ app/serializers/ingredient_serializer.rb
@@ -1,8 +1,8 @@
 # frozen_string_literal: true
 
 class IngredientSerializer < ActiveModel::Serializer
-  attributes :id, :name, :strength, :form, :ing_type, :products, :editable
-  belongs_to :products # , through: :recipe
+  attributes :id, :name, :strength, :form, :ing_type, :products, :recipes, :editable
+  belongs_to :products, through: :recipe
   belongs_to :user
 
   def editable
